d4e5b61bbaa2a5c67317479dcc308afe80f541f9
anton-p-ivanov <anton.p.ivanov@gmail.com>
2019-02-13 14:20:48 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // uncomment to create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // uncomment to define the assets of the project
+    .addEntry('js/app', './assets/js/app.js')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    // .enableBuildNotifications()
+
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
