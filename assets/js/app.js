0a70a4f6b6b9a215d94b4fbc83d35247ac5d377d
anton-p-ivanov <anton.p.ivanov@gmail.com>
2019-03-07 14:47:06 +0300

    Prevent disabled links being clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,28 @@ require('../css/app.scss');
 
     'use strict';
 
+    /**
+     * Handle events
+     * @private
+     */
+    let eventHandler = function (event) {
+
+        let toggle = event.target;
+
+        while (toggle !== document) {
+            // Skip clicks for disabled elements
+            if (toggle.closest('[data-disabled="true"]')) {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+
+                return;
+            }
+
+            toggle = toggle.parentNode;
+        }
+
+    };
+
+    document.addEventListener('click', eventHandler, false);
+
 })();
