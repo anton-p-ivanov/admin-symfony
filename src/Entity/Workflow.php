8163d1d4de08fd26e172a700f8740bb7ab5e73b0
anton-p-ivanov <anton.p.ivanov@gmail.com>
2019-03-07 09:26:21 +0300

    Added class getters and setters

--- src/Entity/Workflow.php
+++ src/Entity/Workflow.php
@@ -64,4 +64,108 @@ class Workflow
      * @ORM\JoinColumn(name="status_uuid", referencedColumnName="uuid", nullable=true)
      */
     private $status;
+
+    /**
+     * Workflow constructor.
+     */
+    public function __construct()
+    {
+        $attributes = [
+            'isDeleted' => false,
+            'createdAt' => new \DateTime(),
+            'updatedAt' => new \DateTime(),
+        ];
+
+        foreach ($attributes as $name => $value) {
+            $this->$name = $value;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return User|null
+     */
+    public function getUpdated(): ?User
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @param User|null $user
+     */
+    public function setUpdated(?User $user): void
+    {
+        $this->updated = $user;
+    }
+
+    /**
+     * @return User|null
+     */
+    public function getCreated(): ?User
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param User|null $user
+     */
+    public function setCreated(?User $user): void
+    {
+        $this->updated = $user;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeleted(): bool
+    {
+        return $this->isDeleted;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return WorkflowStatus|null
+     */
+    public function getStatus(): ?WorkflowStatus
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param WorkflowStatus $status
+     */
+    public function setStatus(WorkflowStatus $status): void
+    {
+        $this->status = $status;
+    }
 }
