f7268b293346648d119d8133c818a4656490a676
anton-p-ivanov <anton.p.ivanov@gmail.com>
2019-03-20 09:26:46 +0300

    Migrations update

--- src/Migrations/Version20190320062549.php
+++ src/Migrations/Version20190320062549.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190226062149 extends AbstractMigration
+final class Version20190320062549 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -31,13 +31,16 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('CREATE TABLE storage_images (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', file_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', width INT NOT NULL, height INT NOT NULL, UNIQUE INDEX UNIQ_DB1C4A6B588338C8 (file_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE storage (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', type CHAR(1) NOT NULL, title VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, content TEXT DEFAULT NULL, UNIQUE INDEX UNIQ_547A1B3494E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE storage_categories_pivot (storage_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', category_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_989F635AD8529BB3 (storage_uuid), INDEX IDX_989F635A5AE42AE1 (category_uuid), PRIMARY KEY(storage_uuid, category_uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE storage_files (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', name VARCHAR(255) NOT NULL, size INT NOT NULL, type VARCHAR(255) NOT NULL, hash CHAR(32) NOT NULL, PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE storage_roles (storage_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', role_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_DBEAE7E7D8529BB3 (storage_uuid), INDEX IDX_DBEAE7E76FC02232 (role_uuid), PRIMARY KEY(storage_uuid, role_uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE storage_files (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', name VARCHAR(255) NOT NULL, size INT NOT NULL, type VARCHAR(255) NOT NULL, hash CHAR(32) NOT NULL, UNIQUE INDEX UNIQ_6BE1897994E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users_accounts (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', user_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', account_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', position VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_B189B576ABFE1C6F (user_uuid), INDEX IDX_B189B5765DECD70C (account_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_passwords (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', user_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', password CHAR(60) NOT NULL, salt CHAR(20) NOT NULL, created_at DATETIME DEFAULT NULL, expired_at DATETIME DEFAULT NULL, is_expired TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_8BFF51F7ABFE1C6F (user_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_checkwords (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', user_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', checkword CHAR(60) NOT NULL, created_at DATETIME DEFAULT NULL, expired_at DATETIME DEFAULT NULL, is_expired TINYINT(1) DEFAULT \'0\' NOT NULL, UNIQUE INDEX UNIQ_1EB3B582DB50E026 (checkword), INDEX IDX_1EB3B582ABFE1C6F (user_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', email VARCHAR(255) NOT NULL, fname VARCHAR(100) NOT NULL, lname VARCHAR(100) NOT NULL, sname VARCHAR(100) DEFAULT NULL, is_active TINYINT(1) DEFAULT \'1\' NOT NULL, is_confirmed TINYINT(1) DEFAULT \'0\' NOT NULL, phone VARCHAR(255) DEFAULT NULL, phone_mobile VARCHAR(255) DEFAULT NULL, skype VARCHAR(255) DEFAULT NULL, birthdate DATE DEFAULT NULL, comments MEDIUMTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), UNIQUE INDEX UNIQ_1483A5E994E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_roles (user_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', role_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_51498A8EABFE1C6F (user_uuid), INDEX IDX_51498A8E6FC02232 (role_uuid), PRIMARY KEY(user_uuid, role_uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_sites (user_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', site_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_5B770E2AABFE1C6F (user_uuid), INDEX IDX_5B770E2AEAE5ED5F (site_uuid), PRIMARY KEY(user_uuid, site_uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE sites (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', is_active TINYINT(1) DEFAULT \'1\' NOT NULL, code VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, sort INT DEFAULT 100 NOT NULL, UNIQUE INDEX UNIQ_BC00AA6377153098 (code), UNIQUE INDEX UNIQ_BC00AA6394E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE accounts (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', title VARCHAR(255) NOT NULL, description MEDIUMTEXT DEFAULT NULL, is_active TINYINT(1) DEFAULT \'1\' NOT NULL, email VARCHAR(255) NOT NULL, web VARCHAR(255) NOT NULL, phone VARCHAR(255) DEFAULT NULL, sort INT DEFAULT 100 NOT NULL, UNIQUE INDEX UNIQ_CAC89EACE7927C74 (email), UNIQUE INDEX UNIQ_CAC89EAC94E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sites (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', is_active TINYINT(1) DEFAULT \'1\' NOT NULL, code VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, description MEDIUMTEXT DEFAULT NULL, url VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, sort INT DEFAULT 100 NOT NULL, UNIQUE INDEX UNIQ_BC00AA6377153098 (code), UNIQUE INDEX UNIQ_BC00AA6394E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE mail_types (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', code VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, description MEDIUMTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_6DF32B5F77153098 (code), UNIQUE INDEX UNIQ_6DF32B5F94E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE mail_templates (uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', type_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', workflow_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', code VARCHAR(255) NOT NULL, sender VARCHAR(255) DEFAULT NULL, recipient VARCHAR(255) NOT NULL, reply_to VARCHAR(255) DEFAULT NULL, copy_to VARCHAR(255) DEFAULT NULL, is_active TINYINT(1) DEFAULT \'1\' NOT NULL, subject VARCHAR(255) NOT NULL, text_body MEDIUMTEXT DEFAULT NULL, html_body MEDIUMTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_17F263ED77153098 (code), INDEX IDX_17F263ED67095AE (type_uuid), UNIQUE INDEX UNIQ_17F263ED94E0409D (workflow_uuid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE mail_templates_sites (template_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', site_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_777628E74B17ACB (template_uuid), INDEX IDX_777628E7EAE5ED5F (site_uuid), PRIMARY KEY(template_uuid, site_uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
@@ -58,6 +61,11 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('ALTER TABLE storage ADD CONSTRAINT FK_547A1B3494E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
         $this->addSql('ALTER TABLE storage_categories_pivot ADD CONSTRAINT FK_989F635AD8529BB3 FOREIGN KEY (storage_uuid) REFERENCES storage (uuid)');
         $this->addSql('ALTER TABLE storage_categories_pivot ADD CONSTRAINT FK_989F635A5AE42AE1 FOREIGN KEY (category_uuid) REFERENCES storage_categories (uuid)');
+        $this->addSql('ALTER TABLE storage_roles ADD CONSTRAINT FK_DBEAE7E7D8529BB3 FOREIGN KEY (storage_uuid) REFERENCES storage (uuid)');
+        $this->addSql('ALTER TABLE storage_roles ADD CONSTRAINT FK_DBEAE7E76FC02232 FOREIGN KEY (role_uuid) REFERENCES roles (uuid)');
+        $this->addSql('ALTER TABLE storage_files ADD CONSTRAINT FK_6BE1897994E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
+        $this->addSql('ALTER TABLE users_accounts ADD CONSTRAINT FK_B189B576ABFE1C6F FOREIGN KEY (user_uuid) REFERENCES users (uuid)');
+        $this->addSql('ALTER TABLE users_accounts ADD CONSTRAINT FK_B189B5765DECD70C FOREIGN KEY (account_uuid) REFERENCES accounts (uuid)');
         $this->addSql('ALTER TABLE users_passwords ADD CONSTRAINT FK_8BFF51F7ABFE1C6F FOREIGN KEY (user_uuid) REFERENCES users (uuid)');
         $this->addSql('ALTER TABLE users_checkwords ADD CONSTRAINT FK_1EB3B582ABFE1C6F FOREIGN KEY (user_uuid) REFERENCES users (uuid)');
         $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E994E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
@@ -65,6 +73,7 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8E6FC02232 FOREIGN KEY (role_uuid) REFERENCES roles (uuid)');
         $this->addSql('ALTER TABLE users_sites ADD CONSTRAINT FK_5B770E2AABFE1C6F FOREIGN KEY (user_uuid) REFERENCES users (uuid)');
         $this->addSql('ALTER TABLE users_sites ADD CONSTRAINT FK_5B770E2AEAE5ED5F FOREIGN KEY (site_uuid) REFERENCES sites (uuid)');
+        $this->addSql('ALTER TABLE accounts ADD CONSTRAINT FK_CAC89EAC94E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
         $this->addSql('ALTER TABLE sites ADD CONSTRAINT FK_BC00AA6394E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
         $this->addSql('ALTER TABLE mail_types ADD CONSTRAINT FK_6DF32B5F94E0409D FOREIGN KEY (workflow_uuid) REFERENCES workflow (uuid)');
         $this->addSql('ALTER TABLE mail_templates ADD CONSTRAINT FK_17F263ED67095AE FOREIGN KEY (type_uuid) REFERENCES mail_types (uuid)');
@@ -81,10 +90,13 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('ALTER TABLE roles DROP FOREIGN KEY FK_B63E2EC794E0409D');
         $this->addSql('ALTER TABLE storage_categories DROP FOREIGN KEY FK_AAA440E694E0409D');
         $this->addSql('ALTER TABLE storage DROP FOREIGN KEY FK_547A1B3494E0409D');
+        $this->addSql('ALTER TABLE storage_files DROP FOREIGN KEY FK_6BE1897994E0409D');
         $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E994E0409D');
+        $this->addSql('ALTER TABLE accounts DROP FOREIGN KEY FK_CAC89EAC94E0409D');
         $this->addSql('ALTER TABLE sites DROP FOREIGN KEY FK_BC00AA6394E0409D');
         $this->addSql('ALTER TABLE mail_types DROP FOREIGN KEY FK_6DF32B5F94E0409D');
         $this->addSql('ALTER TABLE mail_templates DROP FOREIGN KEY FK_17F263ED94E0409D');
+        $this->addSql('ALTER TABLE storage_roles DROP FOREIGN KEY FK_DBEAE7E76FC02232');
         $this->addSql('ALTER TABLE users_roles DROP FOREIGN KEY FK_51498A8E6FC02232');
         $this->addSql('ALTER TABLE storage_categories_pivot DROP FOREIGN KEY FK_989F635A5AE42AE1');
         $this->addSql('ALTER TABLE storage_tree DROP FOREIGN KEY FK_BE1395C996A2E991');
@@ -93,15 +105,18 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('ALTER TABLE storage_requests DROP FOREIGN KEY FK_D84EEA16D8529BB3');
         $this->addSql('ALTER TABLE storage_tree DROP FOREIGN KEY FK_BE1395C9D8529BB3');
         $this->addSql('ALTER TABLE storage_categories_pivot DROP FOREIGN KEY FK_989F635AD8529BB3');
+        $this->addSql('ALTER TABLE storage_roles DROP FOREIGN KEY FK_DBEAE7E7D8529BB3');
         $this->addSql('ALTER TABLE storage_versions DROP FOREIGN KEY FK_BA177C95588338C8');
         $this->addSql('ALTER TABLE storage_images DROP FOREIGN KEY FK_DB1C4A6B588338C8');
         $this->addSql('ALTER TABLE workflow DROP FOREIGN KEY FK_65C59816DE12AB56');
         $this->addSql('ALTER TABLE workflow DROP FOREIGN KEY FK_65C5981616FE72E1');
         $this->addSql('ALTER TABLE storage_requests DROP FOREIGN KEY FK_D84EEA16ABFE1C6F');
+        $this->addSql('ALTER TABLE users_accounts DROP FOREIGN KEY FK_B189B576ABFE1C6F');
         $this->addSql('ALTER TABLE users_passwords DROP FOREIGN KEY FK_8BFF51F7ABFE1C6F');
         $this->addSql('ALTER TABLE users_checkwords DROP FOREIGN KEY FK_1EB3B582ABFE1C6F');
         $this->addSql('ALTER TABLE users_roles DROP FOREIGN KEY FK_51498A8EABFE1C6F');
         $this->addSql('ALTER TABLE users_sites DROP FOREIGN KEY FK_5B770E2AABFE1C6F');
+        $this->addSql('ALTER TABLE users_accounts DROP FOREIGN KEY FK_B189B5765DECD70C');
         $this->addSql('ALTER TABLE users_sites DROP FOREIGN KEY FK_5B770E2AEAE5ED5F');
         $this->addSql('ALTER TABLE mail_templates_sites DROP FOREIGN KEY FK_777628E7EAE5ED5F');
         $this->addSql('ALTER TABLE mail_templates DROP FOREIGN KEY FK_17F263ED67095AE');
@@ -116,12 +131,15 @@ final class Version20190226062149 extends AbstractMigration
         $this->addSql('DROP TABLE storage_images');
         $this->addSql('DROP TABLE storage');
         $this->addSql('DROP TABLE storage_categories_pivot');
+        $this->addSql('DROP TABLE storage_roles');
         $this->addSql('DROP TABLE storage_files');
+        $this->addSql('DROP TABLE users_accounts');
         $this->addSql('DROP TABLE users_passwords');
         $this->addSql('DROP TABLE users_checkwords');
         $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE users_roles');
         $this->addSql('DROP TABLE users_sites');
+        $this->addSql('DROP TABLE accounts');
         $this->addSql('DROP TABLE sites');
         $this->addSql('DROP TABLE mail_types');
         $this->addSql('DROP TABLE mail_templates');
